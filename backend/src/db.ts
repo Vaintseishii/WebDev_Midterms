52ad1e156de62598f7bd5099f3ff62d970f950a5
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-09-24 08:27:19 +0800

    initial commit

---
+++ backend/src/db.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+//import bcrypt from 'bcryptjs';
+
+dotenv.config();
+
+export const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT) || 5432,
+});
